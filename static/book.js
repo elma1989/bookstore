5bdd4eeed75d03227c385c3f8037890815204f44
Marco Elste <marco.elste@web.de>
2025-07-06 15:41:17 +0200

    feat: Added app.js

--- static/book.js
+++ static/book.js
@@ -1,11 +1,13 @@
 export class Book {
-    constructor (bookIndex, title, author, release, price, likes, isbn, comments) {
+    constructor (bookIndex, title, author, release, img, price, likes, isbn, comments) {
         this.bookIndex = bookIndex;
         this.title = title;
         this.author = author;
         this.release = Date(release);
         this.price = price;
+        this.img = img;
         this.likes = likes;
+        this.liked = false;
         this.isbn = isbn;
         this.comments = comments;
     }
